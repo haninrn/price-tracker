59e484589bb03abd081e451b67f3a16154f3ce12
krup13 <khairanafisa4@gmail.com>
2023-12-04 10:57:34 +0800

    wtvr the code says

--- main.java
+++ main.java
@@ -1,5 +1,5 @@
 public class main{
     public static void main(String[] args){
-        System.out.println("the latest changeee");
+        System.out.println("If this message goes through, it means khaira can push the changes");
     }
 }
