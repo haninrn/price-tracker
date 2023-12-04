021ba3ef29892655e7fcb92afa349a15d407c4df
Insyee <insyee@gmail.com>
2023-12-04 14:55:37 +0800

    testt baru

--- main.java
+++ main.java
@@ -2,5 +2,6 @@ public class main{
     public static void main(String[] args){
         System.out.println("If this message goes through, it means khaira can push the changes");
         System.out.println("If this message goes through, it means insyee can push the changes");
+        System.out.println("Hye cantik cantik semua");
     }
 }
